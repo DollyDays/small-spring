7d89adbc21122e53bebf98213a3121a2c6ee989e
DollyDays <DollyDays@qq.com>
2023-03-11 03:47:13 +0800

    重写spring-day01-实现简单的容器

--- src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -1,24 +1,21 @@
 package org.springframework.beans.factory;
 
+import org.springframework.beans.BeanDefinition;
 import org.springframework.beans.BeansException;
 
-public interface BeanFactory {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-    //如果我们在获取Bean的时候，使用&则获取的是FactoryBean本身对象，否则是获取getObject的代理对象。
-    //beanFactory.getBean("helloworld")
-    //beanFactory.getBean("&helloService")  这个获取的是beanFactory对象
-    String FACTORY_BEAN_PREFIX = "&";
+public class BeanFactory {
 
-    Object getBean(String name) throws BeansException;
+    private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
 
-    <T> T getBean(String name, Class<T> requireType) throws BeansException;
+    public Object getBean(String name) {
+        return beanDefinitionMap.get(name).getBean();
+    }
 
-    Object getBean(String name, Object... args) throws BeansException;
-
-
-    <T> T getBean(Class<T> requireType) throws BeansException;
-
-    <T> T getBean(Class<T> requireType, Object... args) throws BeansException;
-
+    public void registerBeanDefinition(String name, BeanDefinition beanDefinition) {
+        beanDefinitionMap.put(name, beanDefinition);
+    }
 
 }
